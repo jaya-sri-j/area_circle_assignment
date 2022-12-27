dea406aeaae864413e72edd60426f797f75491a3
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:33:51 +0530

    adding files.

---
+++ problem44-Area of circle/js/main.js
@@ -0,0 +1,7 @@
+var areaofcircle=prompt("enter the value of area of circle");
+areaofcircle=parseInt(areaofcircle);
+var radius=prompt("enter the value of radius");
+radius=parseInt(radius);
+var areaofcircle;
+var areaofcircle=3.141*radius*radius;
+alert(areaofcircle);
